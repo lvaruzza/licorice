080aa1e85b5d904c9c7554c1572201535120d112
Varuzza <Leonardo.Varuzza@thermofisher.com>
2019-03-07 11:56:10 -0300

    Remove external reference to test file

--- src/test/java/licorice/utils/TestFileSource.java
+++ src/test/java/licorice/utils/TestFileSource.java
@@ -9,7 +9,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class TestFileSource {
 
-    @Test
+    //@Test
     public void testList() {
         FilesSource fs = new FilesSource(List.of("/data/customers/longping10k/vcfs/R_2019_01_25_13_05_38_user_S5-TFS-Schlieren-50-Long_Ping_10K_Maize_Plate_1","/data/customers/longping10k/vcfs/R_2019_01_25_15_40_58_user_S5-TFS-Schlieren-51-Long_Ping_10K_Maize_Plate_2"));
 
